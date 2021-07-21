ae9878b208ecff258f9de029200d9165b7c95df5
Sayyed-Salman <sayyedsalman2000@gmail.com>
2021-07-21 22:51:29 +0530

    firstapp with kivy design

--- firstapp.py
+++ firstapp.py
@@ -4,45 +4,24 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
-class MyGridLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGridLayout, self).__init__(**kwargs)
+class MyGridLayout(Widget):
+    name = ObjectProperty(None)
+    iceCream = ObjectProperty(None)
+    color = ObjectProperty(None)
 
-        self.cols = 2
-
-        # Input Box
-        self.add_widget(Label(text="Name:"))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-
-        self.add_widget(Label(text="Favourite Ice Cream:"))
-        self.iceCream = TextInput(multiline=False)
-        self.add_widget(self.iceCream)
-
-        self.add_widget(Label(text="Favourite Color:"))
-        self.color = TextInput(multiline=False)
-        self.add_widget(self.color)
-
-        self.add_widget(Label(text="Write a Review: "))
-        self.review = TextInput(multiline=True)
-        self.add_widget(self.review)
-
-        # Button
-
-        self.submit = Button(text="Submit", font_size=33)
-        # Bind the Button
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):
+    def press(self):
         name = self.name.text
         iceCream = self.iceCream.text
         color = self.color.text
 
-        self.add_widget(
-            Label(text=f"Hello {name}, you like {iceCream} Ice Cream, and your favourite color is {color}!"))
+        #self.add_widget(
+            #Label(text=f"Hello {name}, you like {iceCream} Ice Cream, and your favourite color is {color}!"))
+
+        print(f"Hello {name}, you like {iceCream} Ice Cream, and your favourite color is {color}!")
 
         self.name.text = ""
         self.iceCream.text = ""
